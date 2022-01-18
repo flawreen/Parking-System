e67956009bf8decc157852710f87ea17cec8c73c
Florin <83332450+flawreen@users.noreply.github.com>
2022-01-18 16:50:10 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iterator>
 #include "parkingLot.h"
 
+/*
 class choices {
 private:
     int rows, columns;
@@ -85,22 +86,23 @@ public:
     }
 
 };
+*/
 
 int main() {
-    chooseOption();
+//     chooseOption();
 
-//    parkingLot p1(7, 7, "La Gica");
-//    Client Florin("Florin");
-//    Client blade("Marius");
-//    Client Gerunziu("Mircea");
-//    p1.parkCar(Florin, 10);
-//    p1.parkCar(blade, 9);
-//    p1.parkCar(Gerunziu, 4);
-//    p1.leaveParkingLot(Florin);
-//    p1.leaveParkingLot(blade);
-//    p1.leaveParkingLot(Gerunziu);
-//    p1.parkCar(Florin, 7);
-//    p1.showParkingLot();
+   parkingLot p1(7, 7, "La Gica");
+   Client Florin("Florin");
+   Client blade("Marius");
+   Client Gerunziu("Mircea");
+   p1.parkCar(Florin, 10);
+   p1.parkCar(blade, 9);
+   p1.parkCar(Gerunziu, 4);
+   p1.leaveParkingLot(Florin);
+   p1.leaveParkingLot(blade);
+   p1.leaveParkingLot(Gerunziu);
+   p1.parkCar(Florin, 7);
+   p1.showParkingLot();
 
     return 0;
 }
